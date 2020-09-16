526b8cf8ca38beba5748aaf64245b2569da0f7e4
Andres Felipe Brieva Pinedo <53844420+BladeNigth@users.noreply.github.com>
2020-09-16 00:03:01 -0500

    Diseño de layout

--- app/src/main/java/co/edu/unimagdalena/apmoviles/tallerunimag2/MainActivity.java
+++ app/src/main/java/co/edu/unimagdalena/apmoviles/tallerunimag2/MainActivity.java
@@ -2,7 +2,7 @@ package co.edu.unimagdalena.apmoviles.tallerunimag2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+        import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
